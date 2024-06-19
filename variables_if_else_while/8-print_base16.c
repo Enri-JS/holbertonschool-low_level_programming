8b6f3933413c67bceed49959a4d183ea8bdc4e82
Enri-JS <danieldejesus9654@gmail.com>
2024-06-18 23:28:03 -0400

    program that prints all the numbers of base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+
+	a = 'a';
+	b = 0;
+	while
+		(b < 10) {
+			putchar(b + '0');
+			b++;
+		}
+	while
+		(a <= 'l') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
