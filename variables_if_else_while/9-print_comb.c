54877dae879f340db074f6fb0191ee06e4e24f30
Enri-JS <danieldejesus9654@gmail.com>
2024-06-18 23:47:17 -0400

    program that prints all possible combinations of single digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c + '0');
+		if (c < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n'0);
+	return (0);
+}
