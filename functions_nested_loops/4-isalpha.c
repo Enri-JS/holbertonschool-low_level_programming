bd542df5a5215c3ecf5659c30423a6a0f3e044b2
Enri-JS <danieldejesus9654@gmail.com>
2024-06-19 21:44:14 -0400

    function that checks for alphabetic character

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - function that checks for alphabetic character
+ * Return: returns to 1 if c is a letter, returns to 0 otherwise
+ * c: character that is being checked
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
