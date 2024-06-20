fecfb88c9b2bd73cd7a2761b2ce48363da261c0f
Enri-JS <danieldejesus9654@gmail.com>
2024-06-20 00:00:09 -0400

    checks for alphabetic character

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - function that checks for alphabetic character
  * Return: returns to 1 if c is a letter, returns to 0 otherwise
- * c: character that is being checked
+ * @c: character that is being checked
  */
 
 int _isalpha(int c)
