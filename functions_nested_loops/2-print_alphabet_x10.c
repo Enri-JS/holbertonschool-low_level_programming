342b5aea13ecddb3440fb1f523efbfcfcc193887
Enri-JS <danieldejesus9654@gmail.com>
2024-06-19 20:51:31 -0400

    prints the alphabet 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * print_alphabet_X10 - prints the alphabet x10 times
+ * print_alphabet_x10 - prints the alphabet x10 times
  * Return: void
  */
 
-void print_alphabet_X10(void)
+void print_alphabet_x10(void)
 {
-	char al;
+	char cl;
 	int c;
 
 	c = 0;
 
 	while (c < 10)
 	{
-		for (al = 'a'; al <= 'z'; al++)
+		for (cl = 'a'; cl <= 'z'; cl++)
 		{
-			_putchar(al);
+			_putchar(cl);
 		}
 	_putchar('\n');
 	c++;
