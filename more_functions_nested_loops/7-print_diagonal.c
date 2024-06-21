6751532edf3b322d0abd629451b968d7d38641ab
Enri-JS <danieldejesus9654@gmail.com>
2024-06-20 23:46:58 -0400

    draws a diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal
+ * @n: number of times
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
