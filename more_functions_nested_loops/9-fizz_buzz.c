b6993398af08524a3a5dba86f6f5879c2def59e2
Enri-JS <danieldejesus9654@gmail.com>
2024-06-21 10:38:54 -0400

    prints FizzBuzz depending on its multiplier from 1 to 100

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints Fizz for mult of 3 and Buzz for mult of 5, FizzBuzz for both
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+			printf("FizzBuzz");
+		else if ((n % 3) == 0)
+			printf("Fizz");
+		else if ((n % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		if (n == 100)
+			continue;
+		printf(" ");
+	}
+	_putchar('\n');
+
+	return (0);
+}
