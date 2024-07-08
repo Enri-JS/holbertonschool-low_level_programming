00112774789f55a7a432db5b90ee298abb1feb75
Enri-JS <danieldejesus9654@gmail.com>
2024-07-08 13:57:44 -0400

    update

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - header file
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+dog_t;
+
+#endif /* DOG_H */
