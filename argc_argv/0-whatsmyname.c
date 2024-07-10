e0db41d36453f9fd9155d8be6a2d57aed9cb7782
Enri-JS <danieldejesus9654@gmail.com>
2024-07-09 20:00:07 -0400

    prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints name
+ * @argv: the array of arguments
+ * @argc: the number of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", *argv);
+
+		return (0);
+}
