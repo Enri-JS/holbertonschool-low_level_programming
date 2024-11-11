a60bacf91c9d5e0953a0a8787845ee70ff3e0fb3
Enri-DJS <danieldejesus9654@gmail.com>
2024-11-10 22:15:45 -0400

    update

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name
+ * @argc: count argument
+ * @argv: vector argument
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
