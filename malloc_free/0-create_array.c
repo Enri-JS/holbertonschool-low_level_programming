ba6afe58e479d445888d64d86bb2f1e88b5d6236
Enri-DJS <danieldejesus9654@gmail.com>
2024-11-24 17:41:02 -0400

    creates an array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/*
+ * create_array - creates an array of chars
+ *  @size: size of the array
+ *  @c: value
+ *  return: NULL if size = 0
+ **/
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
